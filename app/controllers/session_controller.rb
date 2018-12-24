ec29a6a9e73f9ddd635d3fd581ba2b57eace9f3c
ian douglas <ian.douglas@iandouglas.com>
2018-12-23 18:08:32 -0700

    fixes #64, completes #62

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,18 +1,17 @@
 class SessionController < ApplicationController
   def new
+    if current_user
+      flash[:info] = 'You are already logged in'
+      redirect_user(current_user)
+    end
   end
 
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      if user.default?
-        redirect_to profile_path
-      elsif user.merchant?
-        redirect_to dashboard_path
-      elsif user.admin?
-        redirect_to admin_dashboard_index_path
-      end
+      flash[:success] = 'You are logged in'
+      redirect_user(user)
     else
       flash[:error] = 'Email or password is incorrect'
       render :new
@@ -23,5 +22,17 @@ class SessionController < ApplicationController
     session[:user_id] = nil
     flash[:info] = 'You are logged out'
     redirect_to root_path
+  end
+
+  private
+
+  def redirect_user(user)
+    if user.default?
+      redirect_to profile_path
+    elsif user.merchant?
+      redirect_to dashboard_path
+    elsif user.admin?
+      redirect_to admin_dashboard_index_path
+    end
   end
 end
