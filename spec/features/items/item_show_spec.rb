1168c19fde408cb235e9a0867935f405fca32113
ian douglas <ian.douglas@iandouglas.com>
2019-01-01 02:28:37 -0700

    added test for merchants and admins to NOT see Add To Cart button on item show page

--- spec/features/items/item_show_spec.rb
+++ spec/features/items/item_show_spec.rb
@@ -3,26 +3,44 @@ require 'rails_helper'
 include ActionView::Helpers::NumberHelper
 
 RSpec.describe 'Item show page', type: :feature do
-  describe 'as a visitor or registered user' do
-    it 'should show details about a single item' do
-      merchant = create(:merchant)
-      item = create(:item, user: merchant)
-      user = create(:user)
-      order = create(:completed_order, user: user)
-      create(:fulfilled_order_item, order: order, item: item, created_at: 4.days.ago, updated_at: 3.days.ago)
-      create(:fulfilled_order_item, order: order, item: item, created_at: 1.hour.ago, updated_at: 30.minutes.ago)
-
-      visit item_path(item)
-
-      within "#item-#{item.id}" do
-        expect(page).to have_content(item.name)
-        expect(page.find("#item-#{item.id}-image")['src']).to have_content(item.image)
-        expect(page).to have_content("Sold by: #{item.user.name}")
-        expect(page).to have_content("Price: #{number_to_currency(item.price)}")
-        expect(page).to have_content("In stock: #{item.inventory}")
+  before :each do
+    @admin = create(:admin)
+    @merchant = create(:merchant)
+    @item = create(:item, user: @merchant)
+    @user = create(:user)
+    @order = create(:completed_order, user: @user)
+    create(:fulfilled_order_item, order: @order, item: @item, created_at: 4.days.ago, updated_at: 3.days.ago)
+    create(:fulfilled_order_item, order: @order, item: @item, created_at: 1.hour.ago, updated_at: 30.minutes.ago)
+  end
+  describe 'should show details about a single item' do
+    scenario 'as a visitor' do
+      visit item_path(@item)
+      expect(page).to have_button('Add to Cart')
+    end
+    scenario 'as a registered user' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      visit item_path(@item)
+      expect(page).to have_button('Add to Cart')
+    end
+    scenario 'as a merchant' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
+      visit item_path(@item)
+      expect(page).to_not have_button('Add to Cart')
+    end
+    scenario 'as an admin' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+      visit item_path(@item)
+      expect(page).to_not have_button('Add to Cart')
+    end
+    after :each do
+      within "#item-#{@item.id}" do
+        expect(page).to have_content(@item.name)
+        expect(page.find("#item-#{@item.id}-image")['src']).to have_content(@item.image)
+        expect(page).to have_content("Sold by: #{@item.user.name}")
+        expect(page).to have_content("Price: #{number_to_currency(@item.price)}")
+        expect(page).to have_content("In stock: #{@item.inventory}")
         expect(page).to have_content("Average time to fulfill: 12 hours, 15 minutes")
       end
-
     end
   end
 end
