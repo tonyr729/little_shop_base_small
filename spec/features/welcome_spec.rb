af4514efb1f7c0aa6f9823fd9bfac889f447ab65
Tony Robertson <tonyr729@gmail.com>
2019-01-02 15:00:23 -0700

    Fix failing spec for welcome page

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe 'Welcome page', type: :feature do
   it 'displays a simple welcome message' do
     visit root_path
-    expect(page).to have_content('welcome!')
+    expect(page).to have_content('Welcome to our ficticious "Little Shop of Orders" e-commerce platform.')
   end
 end
