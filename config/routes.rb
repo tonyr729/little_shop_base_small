c57b3032c68c5b28f01c0aeba612f59bac0b0c74
Tony Robertson <tonyr729@gmail.com>
2019-01-03 12:13:29 -0700

    Add slug params to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
 
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index, :show], param: :slug
   resources :merchants, only: [:index]
 
   get '/cart', to: 'cart#index'
-  post '/cart/additem/:id', to: 'cart#add_item', as: 'cart_add_item'
-  post '/cart/addmoreitem/:id', to: 'cart#add_more_item', as: 'cart_add_more_item'
+  post '/cart/additem/:slug', to: 'cart#add_item', as: 'cart_add_item'
+  post '/cart/addmoreitem/:slug', to: 'cart#add_more_item', as: 'cart_add_more_item'
   delete '/cart', to: 'cart#destroy', as: 'cart_empty'
-  delete '/cart/item/:id', to: 'cart#remove_more_item', as: 'cart_remove_more_item'
-  delete '/cart/item/:id/all', to: 'cart#remove_all_of_item', as: 'cart_remove_item_all'
+  delete '/cart/item/:slug', to: 'cart#remove_more_item', as: 'cart_remove_more_item'
+  delete '/cart/item/:slug/all', to: 'cart#remove_all_of_item', as: 'cart_remove_item_all'
 
   get '/login', to: 'session#new'
   post '/login', to: 'session#create'
@@ -21,11 +21,11 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'merchants#show', as: 'dashboard'
   namespace :dashboard do
     resources :orders, only: [:show] do
-      patch '/items/:id/fulfill', to: 'orders#fulfill_item', as: 'item_fulfill'
+      patch '/items/:slug/fulfill', to: 'orders#fulfill_item', as: 'item_fulfill'
     end
-    resources :items, except: [:show]
-    patch '/items/:id/enable', to: 'items#enable', as: 'enable_item'
-    patch '/items/:id/disable', to: 'items#disable', as: 'disable_item'
+    resources :items, except: [:show], param: :slug
+    patch '/items/:slug/enable', to: 'items#enable', as: 'enable_item'
+    patch '/items/:slug/disable', to: 'items#disable', as: 'disable_item'
   end
   get '/profile', to: 'profile#index', as: 'profile'
 
@@ -34,8 +34,9 @@ Rails.application.routes.draw do
     resources :orders, only: [:index, :create, :show, :destroy]
   end
 
+  get '/admin/users/:merchant_slug/items', to: 'admin/items#index', as: 'admin_merchant_items', param: :slug
   post '/admin/users/:merchant_id/items', to: 'dashboard/items#create', as: 'admin_user_items'
-  patch '/admin/users/:merchant_id/items/:id', to: 'dashboard/items#update', as: 'admin_user_item'
+  patch '/admin/users/:merchant_id/items/:slug', to: 'dashboard/items#update', as: 'admin_user_item'
   namespace :admin do
     resources :users, only: [:index, :show, :edit], param: :slug do
       patch '/enable', to: 'users#enable', as: 'enable'
@@ -47,7 +48,7 @@ Rails.application.routes.draw do
       patch '/enable', to: 'merchants#enable', as: 'enable'
       patch '/disable', to: 'merchants#disable', as: 'disable'
       patch '/upgrade', to: 'merchants#downgrade', as: 'downgrade'
-      resources :items, only: [:index, :new, :edit]
+      resources :items, only: [:new, :edit], param: :slug
     end
     resources :dashboard, only: [:index]
   end
