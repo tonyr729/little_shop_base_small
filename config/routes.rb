93c7ac8416595cf9c0184f5a7322202944d81c76
Tony Robertson <tonyr729@gmail.com>
2019-01-02 18:27:55 -0700

    Add slug param to all instances of users in routes

--- config/routes.rb
+++ config/routes.rb
@@ -37,13 +37,13 @@ Rails.application.routes.draw do
   post '/admin/users/:merchant_id/items', to: 'dashboard/items#create', as: 'admin_user_items'
   patch '/admin/users/:merchant_id/items/:id', to: 'dashboard/items#update', as: 'admin_user_item'
   namespace :admin do
-    resources :users, only: [:index, :show, :edit] do
+    resources :users, only: [:index, :show, :edit], param: :slug do
       patch '/enable', to: 'users#enable', as: 'enable'
       patch '/disable', to: 'users#disable', as: 'disable'
       patch '/upgrade', to: 'users#upgrade', as: 'upgrade'
       resources :orders, only: [:index, :show]
     end
-    resources :merchants, only: [:show] do
+    resources :merchants, only: [:show], param: :slug do
       patch '/enable', to: 'merchants#enable', as: 'enable'
       patch '/disable', to: 'merchants#disable', as: 'disable'
       patch '/upgrade', to: 'merchants#downgrade', as: 'downgrade'
